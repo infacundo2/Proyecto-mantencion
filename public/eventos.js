6bea0d50a0697f20883ee400ba94efe9e1215be5
infacundo <bamperri2000@gmail.com>
2025-07-03 13:32:38 -0400

    Cargar Zunchadorqas

--- public/eventos.js
+++ public/eventos.js
@@ -26,7 +26,7 @@ import { fechaZunchadora,
          observacionesZunchadora, 
          eliminarMantencionZunchadoraBtn,
          cargarEstadoZunchadora} from './mantenimiento_zunchadoras.js';
-import { zunchadoraSelect } from './zunchadoras.js'; 
+import { zunchadoraSelect, cargarZunchadoras } from './zunchadoras.js'; 
 
 
 
